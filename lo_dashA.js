f270b5f6205efc4fe5ab6144595f06d0f3bae747
Bryan Fernandez <bryfern710@gmail.com>
2020-06-25 15:58:24 -0500

    added more functions built-from-sratch

--- lo_dashA.js
+++ lo_dashA.js
@@ -10,6 +10,7 @@
 const myArray = ['a','b','c','d','e','f'];
 
 const chunk = function(item, index){
+    console.log("chunk function")
     let elementsArray = [];
     let numberOfIterations = 0;
     const finalArray = [];
@@ -50,4 +51,111 @@ const chunk = function(item, index){
     return finalArray;
 }
 
-console.log(chunk(myArray,5))
+// console.log(chunk(myArray,5))
+
+
+
+// Creates an array with all falsey values removed. The values false, null, 0, "", undefined, and NaN are falsey.
+
+// _.compact([0, 1, false, 2 , '', 3]);
+// => [1, 2, 3]
+
+const compactArray = [0, 1, false, 2, '', 3]
+
+const compact = function(inputArray){
+    const falseyValues = [false, null, 0, "", undefined, NaN]
+
+    return inputArray.filter( eachElement => {
+        return !falseyValues.includes(eachElement)
+    })
+}
+
+// console.log(compactArray == compact(compactArray));
+// console.log("compact function")
+// console.log(compact(compactArray))
+
+
+
+// Creates a new array concatenating array with any additional arrays and/or values.
+
+// _.concat(array, [values])
+// returns a new concatenated array
+
+// const concatArray = ['a', 'b', 'c'];
+const concatArray = [1];
+
+const concat = function(inputArray, ...args){
+    return inputArray.concat(...args)
+}
+
+// console.log(concat(concatArray, 2, [3, 5, 6], [[4]]))
+
+
+
+// Creates an array of array values not included in the other given arrays using SameValueZero for equality comparisons. The order and references of result values are determined by the first array.
+
+// _.difference(array, [values]);
+
+const differenceArray = [2, 1, 7];
+
+const difference = function(inputArray, args){
+
+    return inputArray.filter( eachElement => {
+        return !args.includes(eachElement)
+    })
+}
+
+// console.log(difference(differenceArray, [2, 3]))
+
+// Creates a slice of array with n elements dropped from the beginning
+
+//  _.drop(array, [n=1])
+
+const dropArray = [1, 2, 3];
+
+const drop = function(inputArray, n){
+    inputArray.splice(0, n);
+    return inputArray;
+}
+
+// console.log(drop(dropArray));
+
+// Creates a slice of array with n elements dropped from the end.
+
+// _.dropRight(array, [n=1]);
+const dropRightArray = [1, 2, 3];
+
+const dropRight = (inputArray, n) => {
+    if(n === undefined) { n = 1}
+    
+    for(let counter = 0; counter < n; counter++){
+        inputArray.pop()
+    }
+    return inputArray
+}
+// console.log(dropRight(dropRightArray));
+
+// Creates a slice of array excluding elements dropped from the end. Elements are dropped until predicate returns falsey. The predicate is invoked with 3 arguments: (value, index, array)
+
+// _.dropRightWhile(array, [predicate=_.identity])
+
+const users = [
+    {'user': 'barney', 'active': true},
+    {'user': 'fred', 'active': false},
+    {'user': 'pebbles', 'active': false}
+]
+
+const dropRightWhile = (inputArray, iteratee) => {
+    
+    return true;
+}
+
+usersArray = [
+    {'user': 'pebbles', 'active': false },
+    ['active', false],
+    'active'
+]
+
+const oFunction = (o) => {!o.active;}
+
+console.log(dropRightWhile(users, oFunction));
